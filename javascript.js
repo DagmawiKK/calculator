a0e158cdcd6ff563491b822a115c8a90a26baea0
DagmawiKK <dagmawikassa1@gmail.com>
2024-08-22 14:40:09 +0300

    Handle buttons

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 const operate = function(string) {
-    const args = string.split("").filter(char => char != " " && char != "");
+    const args = string.trim().split(" ").filter(char => char != "");
+    if (args.length > 3) {
+        return ("Please input an expression with one operator and two operands")
+    }
 
     args[0] = parseFloat(args[0]);
     args[2] = parseFloat(args[2]);
+
+
     switch(args[1]) {
         case "+":
             return add(args[0], args[2]);
@@ -27,4 +32,32 @@ const multiply = function (...args) {
 const divide = function (...args) {
     return args[0] / args[1];
 }
-console.log(operate("1 + 2 "))
+
+const buttons = document.querySelectorAll("button");
+const input = document.querySelector(".input");
+buttons.forEach((button) => button.addEventListener("click", () => {
+    let previous = 0;
+    let count = 0;
+    if (button.textContent != "clear" && button.textContent != "=") {
+        if (button.textContent != "+" && button.textContent != "-" && button.textContent != "*" && button.textContent != "/") {
+            input.textContent +=  button.textContent; 
+        }
+        else {
+            input.textContent += " " + button.textContent +" ";
+        }
+    }
+    if (button.textContent === "clear") {
+        input.textContent = "";
+
+        count = 0;
+    }
+    if (button.textContent === "=") {
+        if (count === 0) {
+            input.textContent = (operate(input.textContent));
+            count ++;
+        }
+        else {
+            input.textContent = (operate(input.textContent));
+        }
+    }
+}))
