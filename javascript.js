bf13a5852e6f9c57b4e89fd4709c01ee251216d6
DagmawiKK <dagmawikassa1@gmail.com>
2024-08-22 15:10:23 +0300

    Add backspace and handle errors

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
 const operate = function(string) {
     const args = string.trim().split(" ").filter(char => char != "");
+
     if (args.length > 3) {
         return ("Please input an expression with one operator and two operands")
+    }
+
+    if (args.length === 1) {
+        return (args[0]);
     }
 
     args[0] = parseFloat(args[0]);
     args[2] = parseFloat(args[2]);
 
+    if (isNaN(args[0]) || isNaN(args[2])) {
+        return("Invalid Input");
+    }
 
     switch(args[1]) {
         case "+":
@@ -34,30 +42,47 @@ const divide = function (...args) {
 }
 
 const buttons = document.querySelectorAll("button");
-const input = document.querySelector(".input");
+const input = document.querySelector(".expression");
 buttons.forEach((button) => button.addEventListener("click", () => {
     let previous = 0;
     let count = 0;
-    if (button.textContent != "clear" && button.textContent != "=") {
-        if (button.textContent != "+" && button.textContent != "-" && button.textContent != "*" && button.textContent != "/") {
-            input.textContent +=  button.textContent; 
-        }
-        else {
-            input.textContent += " " + button.textContent +" ";
-        }
-    }
-    if (button.textContent === "clear") {
-        input.textContent = "";
 
-        count = 0;
-    }
-    if (button.textContent === "=") {
-        if (count === 0) {
-            input.textContent = (operate(input.textContent));
-            count ++;
-        }
-        else {
-            input.textContent = (operate(input.textContent));
+    if(button.classList.contains("backspace")) {
+        if(input.textContent.length > 0) {
+            input.textContent = input.textContent.slice(0, -1);
         }
     }
+    else {        
+        if (button.textContent != "clear" && button.textContent != "=") {
+            console.log(input.textContent[0])
+            if (input.textContent[0] != "-" && isNaN(parseFloat(input.textContent.trim()[0]))) {
+                input.textContent = "";
+                input.textContent = button.textContent;
+            }
+            else {
+                if (button.textContent != "+" && button.textContent != "-" && button.textContent != "*" && button.textContent != "/") {
+                    input.textContent +=  button.textContent; 
+                }
+                else {
+                    input.textContent += " " + button.textContent +" ";
+                }
+            }
+        }
+        if (button.textContent === "clear") {
+            input.textContent = "";
+            count = 0;
+        }
+        if (button.textContent === "=") {
+            if (count === 0) {
+                input.textContent = (operate(input.textContent));
+                count ++;
+            }
+            else {
+
+                input.textContent = (operate(input.textContent));
+                
+            }
+        }
+    }
+    
 }))
